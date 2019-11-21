3f3c168694f6930862423df43b0af4346fa6d2eb
Middle44 <4middle4@gmail.com>
2019-11-21 08:47:04 +0100

    v2

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+const Map = ol.Map;
+const View = ol.View;
+const ImageLayer = ol.layer.Image;
+const TileLayer = ol.layer.Tile;
+const ImageWMS = ol.source.ImageWMS;
+const OSM = ol.source.OSM;
 const WMSCapabilities = ol.format.WMSCapabilities;
+//////////////////////////////////////////////////
+var layers = [
+  new TileLayer(
+    {
+      source: new OSM()
+    }
+  ),
+];
+var map = new Map(
+  {
+    layers: layers,
+    target: 'map',
+    view: new View(
+      {
+        projection: 'EPSG:4326',
+        center: [17.888738, 48.746600],
+        zoom: 14
+      }
+    )
+  }
+);
+//////////////////////////////////////////////////////
 var parser = new WMSCapabilities();
 const layersToShow = [];
 function Layers() 
@@ -15,26 +43,39 @@ function Layers()
       {
         //console.log(layer.Name, layer.queryable)
       });
-    var table = "<tr><th>Name</th><th>Queryable</th><th>Checkbox</th></tr>";
+    var table = "<th>Name</th><th>Queryable</th><th>Checkbox</th>";
     var rows = data.Capability.Layer.Layer.length;
-    //var cols = test;
     for (var r = 1; r < rows; r++) 
     {
       table += '<tr>';
-      for (var c = 1; c <= 1; c++) 
+      for (var c=1; c<=1; c++) 
       {
-        table += '<tr>' + '<td>' + data.Capability.Layer.Layer[r - 1].Name + '</td>' + '<td>' + data.Capability.Layer.Layer[r - 1].queryable + '</td>' + '<td>' + `<input onclick="showLayer('` + data.Capability.Layer.Layer[r - 1].Name +`')" type = "checkbox"/>` + '</td>' + '</tr>';
+        table += '<tr>'+'<td>'+data.Capability.Layer.Layer[r-1].Name+'</td>'+'<td>'+ data.Capability.Layer.Layer[r-1].queryable +'</td>'+'<td>'+'<input onclick="showLayer('+data.Capability.Layer.Layer[r-1].Name +')"type="checkbox"/>'+'</td>'+'</tr>';
       }
       table += '</tr>'; 
     }
-    document.body.insertAdjacentHTML('beforeend','<table border = "2"> ' + table + '</table>')
+    document.body.insertAdjacentHTML('beforeend','<table border="2">' + table + '</table>')
   })
 }
 function showLayer(layerToShow) 
 {
   layersToShow.push(layerToShow);
 }
-function AddLayers() 
-{  
-  console.log(layersToShow)
+function AddLayers(layers) 
+{
+  var layers = [
+    new ImageLayer(
+      {
+        extent: [17.84506885869866, 48.731926770588124, 17.93695929993104, 48.764736748435276],
+        source: new ImageWMS(
+          {
+            url: 'http://localhost:8080/geoserver/ows?',
+            params: { LAYERS: ['Blasko:Cesta', 'Blasko:Budova', 'Blasko:Chodnik', 'Blasko:Znacka', 'Blasko:Kos', 'Blasko:Park'] },
+            ratio: 1,
+            serverType: 'geoserver'
+          }
+        ),
+      }
+    )
+  ];
 }
